5ce9c79cc4136bda318ee553902ba87ce090e65a
wahyu sumartha priya dharma <wahyusumartha@gmail.com>
2011-10-26 15:23:44 +0800

    add comment

--- controllers/welcome.php
+++ controllers/welcome.php
@@ -2,21 +2,6 @@
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
 	
 	public function __construct(){
 		parent::__construct();
@@ -25,8 +10,10 @@ class Welcome extends CI_Controller {
 	}
 	
 	
-	
 	function login(){
+		/**
+		* Check User has been login or not
+		*/
 		if ($this->instagram->is_login() === TRUE)
 		{
 			print_r($this->instagram->call('/users/self'));
@@ -49,6 +36,9 @@ class Welcome extends CI_Controller {
 	}
 	
 	
+	/**
+	* Callback URL 
+	*/
 	public function callback(){
 		if ($this->instagram->is_login() === TRUE)
 		{
